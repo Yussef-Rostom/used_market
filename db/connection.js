0e5882600871609a0af554093377469d18104476
Yussef-Rostom <yussef.rostom@gmail.com>
2025-08-02 17:38:44 +0300

    add mongodb configuration

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connection = () => {
-  mongoose.connect('mongodb://127.0.0.1:27017/used-market')
+  mongoose.connect('mongodb+srv://yussefrostom:hMGtHgTUpJkgxsWF@used-market.do9ez83.mongodb.net/?retryWrites=true&w=majority&appName=used-market')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err));
 };
