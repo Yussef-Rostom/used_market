c9c6b46a789dd82b2d3734592093272f9ca85316
Yussef-Rostom <yussef.rostom@gmail.com>
2025-08-03 00:22:46 +0300

    auto reconnect with database

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
 
-const connection = () => {
-  mongoose.connect('mongodb+srv://yussefrostom:hMGtHgTUpJkgxsWF@used-market.do9ez83.mongodb.net/?retryWrites=true&w=majority&appName=used-market')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('MongoDB connection error:', err));
-};
+async function connection() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+    });
+    console.log('Connected to MongoDB Atlas');
+  } catch (err) {
+    console.error('Connection error:', err.message);
+    setTimeout(connectToAtlas, 5000);
+  }
+}
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to Atlas');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from Atlas');
+  connectToAtlas();
+});
 
 module.exports = connection;
